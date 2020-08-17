aa84a065cf5f5e3c86546b7127fda01195a57b1b
Hongli Lai <honglilai@gmail.com>
2020-08-17 09:49:34 +0200

    Add editorconfig

--- enable_gcloud_apis.sh
+++ enable_gcloud_apis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
 gcloud services enable \
-	compute.googleapis.com \
-	container.googleapis.com \
-	containerregistry.googleapis.com \
-	dns.googleapis.com \
-	servicenetworking.googleapis.com \
-	storage-component.googleapis.com
+    compute.googleapis.com \
+    container.googleapis.com \
+    containerregistry.googleapis.com \
+    dns.googleapis.com \
+    servicenetworking.googleapis.com \
+    storage-component.googleapis.com
