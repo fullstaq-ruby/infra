d7f1cb9fbaec2d8a00754ea8f64703b8ce2f5344
Hongli Lai <honglilai@gmail.com>
2020-08-19 10:29:41 +0200

    Limit Github Actions service account's access to only the container registry
    Closes #11.

--- terraform/container_registry.tf
+++ terraform/container_registry.tf
@@ -1,8 +1,14 @@
 resource "google_container_registry" "registry" {
 }
 
-resource "google_storage_bucket_iam_binding" "binding" {
+resource "google_storage_bucket_iam_binding" "public-viewable" {
   bucket = "${google_container_registry.registry.bucket_self_link}"
   role   = "roles/storage.objectViewer"
   members = ["allUsers"]
+}
+
+resource "google_storage_bucket_iam_binding" "ci-cd-writable" {
+  bucket = "${google_container_registry.registry.bucket_self_link}"
+  role   = "roles/storage.admin"
+  members = ["serviceAccount:${google_service_account.github-actions.email}"]
 }
--- terraform/service_accounts.tf
+++ terraform/service_accounts.tf
@@ -24,11 +24,6 @@ resource "google_service_account" "github-actions" {
   display_name = "Github Actions"
 }
 
-resource "google_project_iam_member" "github-actions-storage-admin" {
-  role = "roles/storage.admin"
-  member = "serviceAccount:${google_service_account.github-actions.email}"
-}
-
 resource "google_service_account_key" "github-actions-sa-key" {
   service_account_id = "${google_service_account.github-actions.name}"
 }
