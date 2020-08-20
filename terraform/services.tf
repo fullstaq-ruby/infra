63c65877dbbb24f29fd826c90679a91265b98f08
Mijndert Stuij <mijndert@mijndertstuij.nl>
2020-08-20 06:59:45 +0200

    Fix linter error

--- terraform/services.tf
+++ terraform/services.tf
@@ -2,8 +2,7 @@ resource "google_project_service" "gcp_services" {
   count   = length(var.gcp_services)
   project = var.gcloud_project
   service = var.gcp_services[count.index]
-  
-  # (Optional) If true, services that are enabled and which depend on this service should also be disabled when this service is destroyed. 
+  # (Optional) If true, services that are enabled and which depend on this service should also be disabled when this service is destroyed.
   # If false or unset, an error will be generated if any enabled services depend on this service when destroying it.
   disable_dependent_services = true
 }
