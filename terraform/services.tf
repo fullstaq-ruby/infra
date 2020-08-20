daa24b7fbc8a839a7ee78f87e834b3456fd5cc9a
Mijndert Stuij <mijndert@mijndertstuij.nl>
2020-08-20 10:39:22 +0200

    Set disable_dependent_services to false

--- terraform/services.tf
+++ terraform/services.tf
@@ -4,5 +4,5 @@ resource "google_project_service" "gcp_services" {
   service = var.gcp_services[count.index]
   # (Optional) If true, services that are enabled and which depend on this service should also be disabled when this service is destroyed.
   # If false or unset, an error will be generated if any enabled services depend on this service when destroying it.
-  disable_dependent_services = true
+  disable_dependent_services = false
 }
