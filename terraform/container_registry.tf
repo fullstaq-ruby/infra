df67ce8d08d68de117de4fb6b50946dfb0d3de6d
Hongli Lai <honglilai@gmail.com>
2020-08-27 07:48:58 +0200

    Terraform: make some resource names more specific
    So that in future changes we don't clash names.

--- terraform/container_registry.tf
+++ terraform/container_registry.tf
@@ -2,13 +2,13 @@ resource "google_container_registry" "registry" {
   depends_on = [google_project_service.containerregistry-api]
 }
 
-resource "google_storage_bucket_iam_binding" "public-viewable" {
+resource "google_storage_bucket_iam_binding" "container-registry-public-viewable" {
   bucket  = google_container_registry.registry.bucket_self_link
   role    = "roles/storage.objectViewer"
   members = ["allUsers"]
 }
 
-resource "google_storage_bucket_iam_binding" "ci-cd-writable" {
+resource "google_storage_bucket_iam_binding" "container-registry-writable-by-ci-cd" {
   bucket  = google_container_registry.registry.bucket_self_link
   role    = "roles/storage.admin"
   members = ["serviceAccount:${google_service_account.github-actions.email}"]
