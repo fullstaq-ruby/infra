aac27517c5cbc6be2c0ca74230e8205f92c37727
Hongli Lai <honglilai@gmail.com>
2021-04-18 18:57:35 +0200

    Remove unused Kubernetes service account
    It's no longer used after we switched to Autopilot.

--- terraform/service_accounts.tf
+++ terraform/service_accounts.tf
@@ -1,25 +1,3 @@
-resource "google_service_account" "kubernetes" {
-  depends_on   = [google_project_service.iam-api]
-  account_id   = "kubernetes"
-  display_name = "Kubernetes"
-}
-
-resource "google_project_iam_member" "kubernetes-logwriter" {
-  role   = "roles/logging.logWriter"
-  member = "serviceAccount:${google_service_account.kubernetes.email}"
-}
-
-resource "google_project_iam_member" "kubernetes-metricwriter" {
-  role   = "roles/monitoring.metricWriter"
-  member = "serviceAccount:${google_service_account.kubernetes.email}"
-}
-
-resource "google_project_iam_member" "kubernetes-monitoring-viewer" {
-  role   = "roles/monitoring.viewer"
-  member = "serviceAccount:${google_service_account.kubernetes.email}"
-}
-
-
 resource "google_service_account" "server-edition-ci-bot" {
   depends_on   = [google_project_service.iam-api]
   account_id   = "server-edition-ci-bot"
