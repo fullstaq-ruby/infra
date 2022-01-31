c0284d614c4652bbcf76f50d4fd3bdf955f59186
Hongli Lai <honglilai@gmail.com>
2022-01-31 14:45:18 +0100

    Remove unused IP addresses

--- terraform/static_ips.tf
+++ terraform/static_ips.tf
@@ -1,15 +1,3 @@
-# Not yet removing it until DNS caches are flushed
-resource "google_compute_global_address" "apt_gateway" {
-  depends_on = [google_project_service.compute-api]
-  name       = "fullstaq-ruby-packages-apt-gateway"
-}
-
-# Not yet removing it until DNS caches are flushed
-resource "google_compute_global_address" "yum_gateway" {
-  depends_on = [google_project_service.compute-api]
-  name       = "fullstaq-ruby-packages-yum-gateway"
-}
-
 resource "google_compute_global_address" "gateway" {
   depends_on = [google_project_service.compute-api]
   name       = "fullstaq-ruby-packages-gateway"
