c49c751cd5ca00ba4682bff8a0839666c84bef24
Hongli Lai <hongli@hongli.nl>
2024-08-21 19:00:24 +0200

    apiserver: install dependent libraries upon deploy

--- ansible/files/apiserver-deployer.sh
+++ ansible/files/apiserver-deployer.sh
@@ -36,6 +36,11 @@ else
     rm -rf "${TARGET_DIR}.tmp"
     mkdir "${TARGET_DIR}.tmp"
     tar --use-compress-program=unzstd -xf "/tmp/$ASSET_NAME" -C "${TARGET_DIR}.tmp"
+    DPKG_DEPENDENCIES=$(cat "${TARGET_DIR}.tmp/dpkg-dependencies.txt")
+    if [[ -z "$DPKG_DEPENDENCIES" ]]; then
+        echo "Installing dependencies: $DPKG_DEPENDENCIES"
+        apt satisfy -y --no-install-recommends --no-install-suggests "$DPKG_DEPENDENCIES"
+    fi
     rm "/tmp/$ASSET_NAME"
     mv "${TARGET_DIR}.tmp" "$TARGET_DIR"
 
